b692459b8f85badba2ad7f63b5f8dab28dcfb33e
John Tribolet <john@tribolet.info>
2018-08-26 12:08:06 -0400

    Format data for sklearn and vectorize reviews.

--- use-sklearn.py
+++ use-sklearn.py
@@ -1,9 +1,16 @@
-from sklearn.datasets import fetch_20newsgroups
 import mdsd 
 from pathlib import Path
+import numpy
 
 file = Path("../sorted_data/software/all.review")
 
 data = mdsd.parse_file(file)
+# sklearn uses slight different structures
+targets = numpy.array(list(map(lambda x: x['rating'], data)))
+review_text = list(map(lambda x: x['text'], data))
 
-twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(review_text)
